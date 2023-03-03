f0552e5d3269066b1f12ee7025bb222699e00eed
Finnian Anderson <get@finnian.io>
2023-03-04 12:30:21 +1300

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :articles
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "articles#index"
+
+  resources :articles
+end
